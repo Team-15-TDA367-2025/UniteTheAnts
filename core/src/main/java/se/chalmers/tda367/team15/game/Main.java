f0962f5d58b7807e58377eaa7050717db5790006
crisse4 <melvin.christensson@gmail.com>
2025-11-20 23:46:56 +0100

    Colony and Ant
    Used and centered a picture of the colony, created a basic the Colony Class and an even simpler shell of the Ant Class.

--- core/src/main/java/se/chalmers/tda367/team15/game/Main.java
+++ core/src/main/java/se/chalmers/tda367/team15/game/Main.java
@@ -1,6 +1,7 @@
 package se.chalmers.tda367.team15.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -13,14 +14,29 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("AntColony.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        float screenW = Gdx.graphics.getWidth();
+        float screenH = Gdx.graphics.getHeight();
+
+        float imgW = image.getWidth();
+        float imgH = image.getHeight();
+
+        float scale = 0.5f;
+
+        float scaledW = imgW * scale;
+        float scaledH = imgH * scale;
+
+        float x = (screenW - scaledW) / 2f;
+        float y = (screenH - scaledH) / 2f;
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, x, y, scaledW, scaledH);
         batch.end();
     }
 
