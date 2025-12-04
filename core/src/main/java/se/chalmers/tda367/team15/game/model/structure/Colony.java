6523bd956a5306267000b493272a2619e3c5939d
Alve Svarén <alve@hotmail.se>
2025-12-04 16:09:49 +0100

    Remove singleton

--- core/src/main/java/se/chalmers/tda367/team15/game/model/structure/Colony.java
+++ core/src/main/java/se/chalmers/tda367/team15/game/model/structure/Colony.java
@@ -3,13 +3,10 @@ package se.chalmers.tda367.team15.game.model.structure;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.badlogic.gdx.math.GridPoint2;
 
-import se.chalmers.tda367.team15.game.model.GameWorld;
 import se.chalmers.tda367.team15.game.model.interfaces.TimeObserver;
 import se.chalmers.tda367.team15.game.model.AttackCategory;
 import se.chalmers.tda367.team15.game.model.CanBeAttacked;
@@ -37,7 +34,6 @@ public class Colony extends Structure implements CanBeAttacked, EntityDeathObser
         this.inventory = new Inventory(1000); // test value for now
         // Register to receive ant death notifications
         DestructionListener.getInstance().addEntityDeathObserver(this);
-        GameWorld.getInstance().addTimeObserver(this);
     }
 
     public void addAnt(Ant ant) {
